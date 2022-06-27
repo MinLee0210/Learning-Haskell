040476048ea673d305e79dbaf61cd642e7e89e4e
minhleduc0210 <bastreet124@gmail.com>
2022-06-27 21:06:37 +0700

    Updated

---
+++ Chapter 1/example_7.hs
@@ -0,0 +1,19 @@
+-- LEARNING RECURSIVE
+myGCD a b = if remainder == 0
+            then b
+            else myGCD b remainder
+    where remainder = a `mod` b
+
+sayAmount n = case n of 
+            1 -> "one"
+            2 -> "two"
+            n -> "a bunch"
+
+isEmpty [] = True
+isEmpty _ = False
+
+myHead (x:xs) = x
+myHead [] = error "No head for empty list"
+
+myTail (_:xs) = xs
+myTail [] = error "No tail for empty list"
